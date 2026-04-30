fe0920936e66ca9502025d6f8d09a5becce56b07
mandirigma009 <warriorserver009@gmail.com>
2026-05-01 07:46:52 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: "https://classroommanagement.online",
-        target: "http://localhost:5000",
+         target: "https://classroommanagement.online",
+       // target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
